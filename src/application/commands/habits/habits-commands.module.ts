7463f43979630b0e0598e48027f245e292cc4a89
PedroAugSouza <psouza.880@icloud.com>
2025-02-25 06:29:22 -0300

    feat: implement habits controllers and use cases in command module

--- src/application/commands/habits/habits-commands.module.ts
+++ src/application/commands/habits/habits-commands.module.ts
@@ -1,7 +1,17 @@
 import { Module } from '@nestjs/common';
+import { CreateHabitsUseCase } from './create/create-habits.use-case';
+import { RemoveHabitsUseCase } from './remove/remove-habits.use-case';
+import { UpdateHabitsUseCase } from './update/update-habits.use-case';
+import { CreateHabitController } from './create/create-habits.controller';
+import { RemoveHabitsController } from './remove/remove-habits.controller';
+import { UpdateHabitController } from './update/update-habit.controller';
 
 @Module({
-  providers: [],
-  controllers: [],
+  providers: [CreateHabitsUseCase, RemoveHabitsUseCase, UpdateHabitsUseCase],
+  controllers: [
+    CreateHabitController,
+    RemoveHabitsController,
+    UpdateHabitController,
+  ],
 })
 export class HabitsCommandsModule {}
