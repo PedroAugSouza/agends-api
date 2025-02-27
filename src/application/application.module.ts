6c9f9ecd21f0bd49fa6adde20cd80100ef3486fb
PedroAugSouza <psouza.880@icloud.com>
2025-02-27 19:56:23 -0300

    feat: add auth module in application module

--- src/application/application.module.ts
+++ src/application/application.module.ts
@@ -2,8 +2,9 @@ import { Module } from '@nestjs/common';
 import { CommandsModule } from './commands/commands.module';
 import { PrismaModule } from 'src/infra/prisma/prisma.module';
 import { PrismaRepositoriesModule } from 'src/infra/repositories/prisma-repositories.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [CommandsModule, PrismaModule, PrismaRepositoriesModule],
+  imports: [CommandsModule, PrismaModule, PrismaRepositoriesModule, AuthModule],
 })
 export class AppModule {}
